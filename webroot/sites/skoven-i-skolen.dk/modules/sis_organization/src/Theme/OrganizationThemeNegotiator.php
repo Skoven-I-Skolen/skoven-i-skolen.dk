c9e561280ca0f9076c1370aea429db5b30d68478
Peter Vesterkær <peter@vesterkaer.com>
2022-06-21 08:27:19 +0200

    SIS2-98: Only show frontend theme for organizations

--- webroot/sites/skoven-i-skolen.dk/modules/sis_organization/src/Theme/OrganizationThemeNegotiator.php
+++ webroot/sites/skoven-i-skolen.dk/modules/sis_organization/src/Theme/OrganizationThemeNegotiator.php
@@ -7,12 +7,15 @@ use Drupal\Core\Theme\ThemeNegotiatorInterface;
 
 class OrganizationThemeNegotiator implements ThemeNegotiatorInterface {
 
+  /**
+   * @inheritDoc
+   */
   public function applies(RouteMatchInterface $route_match) {
     if ($routeObject = $route_match->getRouteObject()) {
       $path = $routeObject->getPath();
       $currentUser = \Drupal::currentUser();
 
-      if ($path === '/node/add/{node_type}' && in_array('organization', $currentUser->getRoles(), TRUE)) {
+      if (($path === '/node/add/{node_type}' || $path === '/node/{node}/edit') && in_array('organization', $currentUser->getRoles(), TRUE)) {
         return TRUE;
       }
     }
@@ -20,6 +23,9 @@ class OrganizationThemeNegotiator implements ThemeNegotiatorInterface {
     return FALSE;
   }
 
+  /**
+   * @inheritDoc
+   */
   public function determineActiveTheme(RouteMatchInterface $route_match) {
     return 'sis';
   }
