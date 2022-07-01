1fb1fb7925992bb3582949dc68832aecd214da32
Simas Raudonis <srd@novicell.dk>
2022-07-01 08:12:38 +0200

    SIS2-198: bound button to seasonal wheel, created animation that goes through months - to current month.

--- webroot/sites/skoven-i-skolen.dk/themes/custom/sis/assets/components/js/seasonal-wheel.js
+++ webroot/sites/skoven-i-skolen.dk/themes/custom/sis/assets/components/js/seasonal-wheel.js
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
     el: '.seasonal-wheel',
     data: {
       seasonalWheelData: {},
+      isLoading: false,
     },
     methods: {
       async getSeasonalArticles() {
@@ -29,6 +30,14 @@ document.addEventListener('DOMContentLoaded', () => {
             console.log(error);
           });
       },
+      seasonWheelController() {
+        if (this.isLoading) {
+          console.log('isLoading');
+        } else {
+          this.resetHover();
+          this.addHoverClasses();
+        }
+      },
       addHoverClasses() {
         // Month Hover classes
         const Jan = document.querySelector('.st24');
@@ -44,30 +53,73 @@ document.addEventListener('DOMContentLoaded', () => {
         const Nov = document.querySelector('.st4');
         const Dec = document.querySelector('.st1');
 
-        // Dec.classList.add('hover-wheel');
-        // Nov.classList.add('hover-wheel');
-        // Oct.classList.add('hover-wheel');
-        // Sept.classList.add('hover-wheel');
-        // August.classList.add('hover-wheel');
-        // July.classList.add('hover-wheel');
-        // June.classList.add('hover-wheel');
-        // May.classList.add('hover-wheel');
-        // April.classList.add('hover-wheel');
-        // March.classList.add('hover-wheel');
-        // Feb.classList.add('hover-wheel');
-        // Jan.classList.add("hover-wheel");
+        //    go through all the months and add the hover classList
+        const months = [
+          Jan,
+          Feb,
+          March,
+          April,
+          May,
+          June,
+          July,
+          August,
+          Sept,
+          Oct,
+          Nov,
+          Dec,
+        ];
+
+        // get current month
+        const currentMonth = new Date().getMonth();
+        const currentMonthIndex = currentMonth + 1;
+
+        // Stop loop at the current month
+        for (let i = 0; i < currentMonthIndex; i += 1) {
+          setTimeout(() => {
+            months[i].classList.add('hover-wheel');
+          }, i * 200);
+          setTimeout(() => {
+            if (i === currentMonthIndex - 1) {
+              months[i].classList.add('hover-wheel');
+            } else {
+              months[i].classList.remove('hover-wheel');
+            }
+          }, i * 200 + 200);
+        }
+      },
+      resetHover() {
+        // Month Hover classes
+        const Jan = document.querySelector('.st24');
+        const Feb = document.querySelector('.st22');
+        const March = document.querySelector('.st20');
+        const April = document.querySelector('.st18');
+        const May = document.querySelector('.st16');
+        const June = document.querySelector('.st14');
+        const July = document.querySelector('.st12');
+        const August = document.querySelector('.st10');
+        const Sept = document.querySelector('.st8');
+        const Oct = document.querySelector('.st6');
+        const Nov = document.querySelector('.st4');
+        const Dec = document.querySelector('.st1');
 
         //    go through all the months and add the hover classList
-        const months = [Jan, Feb, March, April, May, June, July, August, Sept, Oct, Nov, Dec];
-        months.forEach((month, i) => {
-          setTimeout(() => {
-            month.classList.add('hover-wheel');
-          },
-          i * 300);
-          setTimeout(() => {
-            month.classList.remove('hover-wheel');
-          },
-          i * 300 + 300);
+        const months = [
+          Jan,
+          Feb,
+          March,
+          April,
+          May,
+          June,
+          July,
+          August,
+          Sept,
+          Oct,
+          Nov,
+          Dec,
+        ];
+        // remove all classes from the hover classList
+        months.forEach((month) => {
+          month.classList.remove('hover-wheel');
         });
       },
     },
