705d1886e0e7069ee61e9b3ace32cbae18d6ddcc
Peter Vesterkær <peter@vesterkaer.com>
2022-07-06 09:56:26 +0200

    SIS2-77: Added styling of newspage

---
+++ webroot/sites/skoven-i-skolen.dk/modules/sis_news/src/Services/NewsContentDeliveryService.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\sis_news\Services;
+
+use Drupal\Core\Entity\EntityTypeManager;
+use Drupal\Core\Entity\EntityViewBuilder;
+use Drupal\node\Entity\Node;
+use Drupal\node\NodeInterface;
+use Drupal\sis_news\Repository\NewsRepository;
+
+class NewsContentDeliveryService {
+
+  /**
+   * @var \Drupal\sis_news\Repository\NewsRepository
+   */
+  private NewsRepository $newsRepository;
+
+  /**
+   * @var \Drupal\Core\Entity\EntityViewBuilder
+   */
+  private EntityViewBuilder $entityViewBuilder;
+
+  public function __construct(NewsRepository $newsRepository, EntityTypeManager $entityTypeManager) {
+    $this->newsRepository = $newsRepository;
+    $this->entityViewBuilder = $entityTypeManager->getViewBuilder('node');
+  }
+
+  /**
+   * Get Latest news
+   *
+   * @return array
+   *   Array of rendered news items.
+   */
+  public function getLatestNews(NodeInterface $node): array {
+    if ($latest = $this->newsRepository->getLatestNews($node)) {
+      $nodes = Node::loadMultiple($latest);
+      return $this->entityViewBuilder->viewMultiple($nodes,'list');
+    }
+
+    return [];
+  }
+}
