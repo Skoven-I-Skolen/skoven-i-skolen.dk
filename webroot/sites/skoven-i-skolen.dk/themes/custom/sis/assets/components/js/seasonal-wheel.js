04c35c338ef0a075b8dc2e24ee8f06f42de2d6d2
Simas Raudonis <srd@novicell.dk>
2022-07-08 09:34:50 +0200

    SIS:198 Added keypress (esc) event listener to modal, so on esc when modal is open user can esc close modal.

--- webroot/sites/skoven-i-skolen.dk/themes/custom/sis/assets/components/js/seasonal-wheel.js
+++ webroot/sites/skoven-i-skolen.dk/themes/custom/sis/assets/components/js/seasonal-wheel.js
@@ -98,7 +98,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
             // remove hidden class from modal to show it -
             // this is used as workaround because of modal flicering on page load
-            document.querySelector('.hidden-modal').classList.remove('hidden-modal');
+            document
+              .querySelector('.hidden-modal')
+              .classList.remove('hidden-modal');
           });
         }, 1200);
       },
@@ -307,37 +309,48 @@ document.addEventListener('DOMContentLoaded', () => {
         if (currentMonthIndex === 1) {
           this.currentMonth = 'january';
           return 'january';
-        } if (currentMonthIndex === 2) {
+        }
+        if (currentMonthIndex === 2) {
           this.currentMonth = 'february';
           return 'february';
-        } if (currentMonthIndex === 3) {
+        }
+        if (currentMonthIndex === 3) {
           this.currentMonth = 'march';
           return 'march';
-        } if (currentMonthIndex === 4) {
+        }
+        if (currentMonthIndex === 4) {
           this.currentMonth = 'april';
           return 'april';
-        } if (currentMonthIndex === 5) {
+        }
+        if (currentMonthIndex === 5) {
           this.currentMonth = 'may';
           return 'may';
-        } if (currentMonthIndex === 6) {
+        }
+        if (currentMonthIndex === 6) {
           this.currentMonth = 'june';
           return 'june';
-        } if (currentMonthIndex === 7) {
+        }
+        if (currentMonthIndex === 7) {
           this.currentMonth = 'july';
           return 'july';
-        } if (currentMonthIndex === 8) {
+        }
+        if (currentMonthIndex === 8) {
           this.currentMonth = 'august';
           return 'august';
-        } if (currentMonthIndex === 9) {
+        }
+        if (currentMonthIndex === 9) {
           this.currentMonth = 'september';
           return 'september';
-        } if (currentMonthIndex === 10) {
+        }
+        if (currentMonthIndex === 10) {
           this.currentMonth = 'october';
           return 'october';
-        } if (currentMonthIndex === 11) {
+        }
+        if (currentMonthIndex === 11) {
           this.currentMonth = 'november';
           return 'november';
-        } if (currentMonthIndex === 12) {
+        }
+        if (currentMonthIndex === 12) {
           this.currentMonth = 'december';
           return 'december';
         }
@@ -353,11 +366,23 @@ document.addEventListener('DOMContentLoaded', () => {
         this.showModal = true;
         document.querySelector('body').classList.add('overflow-hidden');
       },
+
+      onKeyPress(e) {
+        if (e.keyCode === 27) {
+          this.closeModal();
+        }
+      },
     },
 
     mounted() {
       this.getCurrentMonth();
       this.addHoverEffectOnCurrentMonth(this.currentMonth);
+    },
+    created() {
+      window.addEventListener('keyup', this.onKeyPress);
+    },
+    beforeDestroy() {
+      window.removeEventListener('keyup', this.onKeyPress);
     },
   });
 });
