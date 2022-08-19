f3554eeec8051e618c33d79495572aea878796a8
Søren Due Bonde <sdb@novicell.dk>
2022-08-19 13:48:34 +0200

    SIS2-25: Changed slideshow to use Swiper instead of Flickity

--- webroot/sites/skoven-i-skolen.dk/themes/custom/sis/assets/components/js/entity-slideshow.js
+++ webroot/sites/skoven-i-skolen.dk/themes/custom/sis/assets/components/js/entity-slideshow.js
@@ -3,45 +3,44 @@
  * Image slideshow.
  */
 
-import Flickity from 'flickity';
-import FlickityFade from 'flickity-fade';
-import FlickityAsNavFor from 'flickity-as-nav-for';
-import 'flickity/css/flickity.css';
+import Swiper, { Autoplay, Pagination, Lazy } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/pagination';
 
 Drupal.behaviors.entitySlideshow = {
-  attach(context) {
+  attach() {
     const entitySlideShowsWrappers = document.querySelectorAll('.js-entity-slideshow-wrapper:not(.loaded)');
-    if (entitySlideShowsWrappers.length === 0) {
-      return;
-    }
-
     for (let i = 0; i < entitySlideShowsWrappers.length; i += 1) {
-      const current = entitySlideShowsWrappers[i];
-      const slideshow = current.querySelector('.js-entity-slideshow');
-      const navigation = current.querySelector('.js-entity-slideshow-navigation');
-      current.classList.add('loaded');
-
-      setTimeout(() => {
-        const flktySlideshow = new Flickity(slideshow, {
-          // options
-          cellAlign: 'left',
-          contain: true,
-          pageDots: true,
-          prevNextButtons: false,
-          fade: true,
-          wrapAround: true,
-          autoPlay: current.dataset.autoplay === 'true' ? 3000 : '',
-          cellSelector: '.entity-slideshow-slide',
+      const slideshow = entitySlideShowsWrappers[i];
+      slideshow.classList.add('loaded');
+      // Build slider
+      if (slideshow.dataset.autoplay === 'true') {
+        const swiperSlide = new Swiper(slideshow, {
+          modules: [Autoplay, Pagination, Lazy],
+          lazy: true,
+          loop: true,
+          slideClass: 'entity-slideshow-slide',
+          pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+          },
+          autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+          },
         });
-
-        const flktyNavigation = new Flickity(navigation, {
-          // options
-          contain: true,
-          pageDots: false,
-          prevNextButtons: false,
-          asNavFor: slideshow,
+      } else {
+        const swiperSlide = new Swiper(slideshow, {
+          modules: [Autoplay, Pagination, Lazy],
+          lazy: true,
+          loop: true,
+          slideClass: 'entity-slideshow-slide',
+          pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+          },
         });
-      });
+      }
     }
   },
 };
