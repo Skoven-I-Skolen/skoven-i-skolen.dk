5131a9797014457917dba7963be0b7fc10ee45f3
Simas Raudonis <srd@novicell.dk>
2022-10-03 16:04:04 +0200

    SIS-277: Added x out of y for the ALL carousels, translations for map buttons, styling.

--- webroot/sites/skoven-i-skolen.dk/themes/custom/sis/assets/components/js/related-articles.js
+++ webroot/sites/skoven-i-skolen.dk/themes/custom/sis/assets/components/js/related-articles.js
@@ -3,9 +3,10 @@
  * Inline navigation.
  */
 
-import Swiper, { Navigation, Lazy } from 'swiper';
+import Swiper, { Navigation, Lazy, Pagination } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 Drupal.behaviors.relatedArticles = {
   attach() {
@@ -15,12 +16,23 @@ Drupal.behaviors.relatedArticles = {
       slideList.classList.add('loaded');
       // Build slider
       const swiper = new Swiper(slideList.querySelector('.js-article-page__related-items'), {
-        modules: [Navigation, Lazy],
+        modules: [Navigation, Lazy, Pagination],
         slidesPerView: 'auto',
         lazy: true,
         watchOverflow: true,
         slideClass: 'inspiration-overview__item',
         slideVisibleClass: 'swiper-slide-visible',
+        currentClass: 'swiper-slide-active',
+        pagination: {
+          el: '.swiper-pagination',
+          type: 'fraction',
+          formatFractionCurrent(number) {
+            return number.toString().padStart(2, '');
+          },
+          formatFractionTotal(number) {
+            return number.toString().padStart(2, '');
+          },
+        },
         navigation: {
           nextEl: '.swiper-button-next',
           prevEl: '.swiper-button-prev',
