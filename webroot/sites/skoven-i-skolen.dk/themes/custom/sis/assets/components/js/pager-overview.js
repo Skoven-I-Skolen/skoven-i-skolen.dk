141a9a940c768a53144c1571d37d3bc337a05b87
Simas Raudonis <srd@novicell.dk>
2022-10-13 16:06:14 +0200

    A bunch of changes for SIS2-170.

--- webroot/sites/skoven-i-skolen.dk/themes/custom/sis/assets/components/js/pager-overview.js
+++ webroot/sites/skoven-i-skolen.dk/themes/custom/sis/assets/components/js/pager-overview.js
@@ -13,7 +13,6 @@ Drupal.behaviors.pageUp = {
         // Scroll to class
         scrollToClass() {
           const el = document.querySelector('.overview-form__header');
-          console.log('scrollToClass');
           el.scrollIntoView({
           });
         },
@@ -24,8 +23,6 @@ Drupal.behaviors.pageUp = {
         const url = window.location.href;
         if (url.indexOf('page') > -1) {
           this.scrollToClass();
-        } else {
-          console.log('no page');
         }
       },
     });
