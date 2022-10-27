cdab016b9532562c794f6cf4633e559d699535df
Søren Due Bonde <sdb@novicell.dk>
2022-10-27 17:24:25 +0200

    SIS2-277: Fix latest and related swiper slider navigation count and next and prev functionality

--- webroot/sites/skoven-i-skolen.dk/themes/custom/sis/assets/components/js/latest-news.js
+++ webroot/sites/skoven-i-skolen.dk/themes/custom/sis/assets/components/js/latest-news.js
@@ -3,10 +3,9 @@
  * Inline navigation.
  */
 
-import Swiper, { Navigation, Lazy, Pagination } from 'swiper';
+import Swiper, { Navigation, Lazy } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/navigation';
-import 'swiper/css/pagination';
 
 Drupal.behaviors.latestNews = {
   attach() {
@@ -16,27 +15,19 @@ Drupal.behaviors.latestNews = {
       slideList.classList.add('loaded');
       const sliderPagerTotal = slideList.querySelector('.slider-pager-count--total');
       const sliderPagerShowing = slideList.querySelector('.slider-pager-count--showing');
+      const sliderNext = slideList.querySelector('.swiper-button-next');
+      const sliderPrev = slideList.querySelector('.swiper-button-prev');
       // Build slider
       const swiper = new Swiper(slideList.querySelector('.js-news-page__latest-items'), {
-        modules: [Navigation, Lazy, Pagination],
+        modules: [Navigation, Lazy],
         slidesPerView: 'auto',
         lazy: true,
         watchOverflow: true,
         slideClass: 'inspiration-overview__item',
         slideVisibleClass: 'swiper-slide-visible',
-        pagination: {
-          el: '.swiper-pagination',
-          type: 'fraction',
-          formatFractionCurrent(number) {
-            return number.toString().padStart(2, '');
-          },
-          formatFractionTotal(number) {
-            return number.toString().padStart(2, '');
-          },
-        },
         navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
+          nextEl: sliderNext,
+          prevEl: sliderPrev,
           disabledClass: 'swiper-button-disabled',
         },
         on: {
