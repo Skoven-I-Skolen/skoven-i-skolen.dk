d60fcb741adfeed636c445e18239dd4a52353520
Søren Due Bonde <sdb@novicell.dk>
2023-11-24 13:08:16 +0100

    Fix Call to undefined method Drupal\profile\ProfileStorage::accessCheck() in /sites/skoven-i-skolen.dk/modules/sis_organization/src/Repository/OrganizationRepository.php).

--- webroot/sites/skoven-i-skolen.dk/modules/sis_organization/src/Repository/OrganizationRepository.php
+++ webroot/sites/skoven-i-skolen.dk/modules/sis_organization/src/Repository/OrganizationRepository.php
@@ -37,7 +37,7 @@ class OrganizationRepository {
 
 
   public function getNumberOfOrganizations() {
-    return $this->entityTypeManager->getStorage('profile')->accessCheck(FALSE)
+    return $this->entityTypeManager->getStorage('profile')
       ->getQuery()
       ->accessCheck(TRUE)
       ->condition('status', NodeInterface::PUBLISHED)
