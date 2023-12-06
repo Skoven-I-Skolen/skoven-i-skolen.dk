54ac23b64183e7edfdb5c5c1c762e82428f77805
Søren Due Bonde <sdb@novicell.dk>
2023-12-07 00:02:47 +0100

    Fix "Se alle skribenter" button does not work on blog posts

--- webroot/sites/skoven-i-skolen.dk/modules/sis_blog_post/js/sis_blog_post.js
+++ webroot/sites/skoven-i-skolen.dk/modules/sis_blog_post/js/sis_blog_post.js
@@ -1,50 +1,31 @@
-(function($) {
-  Drupal.behaviors.sis_blog_post = {
-    attach: function(context, settings) {
-      var show_all_link = document.querySelector('.view-all-writers-link');
-      if (show_all_link) {
-        show_all_link.addEventListener('click', function (event) {
-          hideOrDisplayAllWritersRegion();
-          if (show_all_link.classList.contains('hidden')) {
-            show_all_link.classList.remove('hidden');
-          }
-          else {
-            show_all_link.classList.add('hidden');
-          }
-        });
-      }
-      var hide_all_link = document.querySelector('.hide-all-writers-link');
-      if (hide_all_link) {
-        hide_all_link.addEventListener('click', function (event) {
-          hideOrDisplayAllWritersRegion();
-          var show_all_link = document.querySelector('.view-all-writers-link');
-          if (show_all_link) {
-            if (show_all_link.classList.contains('hidden')) {
-              show_all_link.classList.remove('hidden');
-            }
-          }
-        });
-      }
+document.addEventListener('DOMContentLoaded', () => {
+  const bloggersWriters = document.querySelector('.blog-post--view-all-writers');
+  const bloggersTrigger = document.querySelector('.view-all-writers-link');
+  const bloggersHideTrigger = document.querySelector('.hide-all-writers-link');
+  if (bloggersWriters.length === 0) {
+    return;
+  }
+  bloggersTrigger.addEventListener('click', () => {
+    bloggersTrigger.classList.toggle('hidden');
+    bloggersWriters.classList.toggle('expanded');
+    var headerOffset = 180;
+    var elementPosition = bloggersWriters.getBoundingClientRect().top;
+    var offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+    window.scrollTo({
+      top: offsetPosition,
+      behavior: "smooth"
+    });
+  });
 
-      function hideOrDisplayAllWritersRegion() {
-        var allWritersRegion = document.querySelector('.blog-post--view-all-writers');
-        if (allWritersRegion) {
-          if (allWritersRegion.classList.contains('expanded')) {
-            allWritersRegion.classList.remove('expanded');
-          }
-          else {
-            allWritersRegion.classList.add('expanded');
-            var headerOffset = 180;
-            var elementPosition = allWritersRegion.getBoundingClientRect().top;
-            var offsetPosition = elementPosition + window.pageYOffset - headerOffset;
-            window.scrollTo({
-              top: offsetPosition,
-              behavior: "smooth"
-            });
-          }
-        }
-      }
-
-    }
-  };
-})(jQuery);
+  bloggersHideTrigger.addEventListener('click', () => {
+    bloggersTrigger.classList.toggle('hidden');
+    bloggersWriters.classList.toggle('expanded');
+    var headerOffset = 180;
+    var elementPosition = bloggersTrigger.getBoundingClientRect().top;
+    var offsetPosition = elementPosition + window.pageYOffset - headerOffset;
+    window.scrollTo({
+      top: offsetPosition,
+      behavior: "smooth"
+    });
+  });
+});
