bbc3279cc753946ee9e626b8d43fb02949045cb1
Erik Petra <epe@novicell.dk>
2024-02-05 15:14:27 +0100

    SIS2-811: Fixed editor filters for CKEditor 5

--- webroot/sites/skoven-i-skolen.dk/modules/sis_misc/src/Plugin/Filter/ResponsiveTable.php
+++ webroot/sites/skoven-i-skolen.dk/modules/sis_misc/src/Plugin/Filter/ResponsiveTable.php
@@ -11,7 +11,7 @@ use Drupal\filter\Plugin\FilterBase;
  * @Filter(
  *   id = "filter_responsive_table",
  *   title = @Translation("Responsive table"),
- *   type = Drupal\filter\Plugin\FilterInterface::TYPE_MARKUP_LANGUAGE,
+ *   type = Drupal\filter\Plugin\FilterInterface::TYPE_TRANSFORM_REVERSIBLE,
  * )
  */
 class ResponsiveTable extends FilterBase {
